b8e94cffcea9cc55b387ee0fd8fa705033de5038
Kim-Kwan-Ho <rlarhksgh001@gmail.com>
2024-08-07 12:51:19 +0900

    Chicken Prefab Changed

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : BaseBehaviour
 {
     [Header("Movement Settings")]
-    [SerializeField]private float _moveSpeed = 5f;
-    [SerializeField]private float _turnSpeed = 180f;
-    private Rigidbody _rb;
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _turnSpeed = 180f;
+    [SerializeField] private Rigidbody _rb;
     private bool _onGround = true;
+
+    [Header("Animation Settings")]
+    [SerializeField] private Animator _animator;
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
     }
     void FixedUpdate()
     {
@@ -28,6 +30,14 @@ public class PlayerMovement : MonoBehaviour
         float move = Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime;
         float turn = Input.GetAxis("Horizontal") * _turnSpeed * Time.deltaTime;
 
+        if (move != 0 || turn != 0)
+        {
+            _animator.SetInteger(AnimationSettings.Walk, 1);
+        }
+        else
+        {
+            _animator.SetInteger(AnimationSettings.Walk, 0);
+        }
         Vector3 newVelocity = transform.forward * move * 100;
         newVelocity.y = _rb.velocity.y;
         _rb.velocity = newVelocity;
@@ -41,14 +51,15 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && _onGround)
         {
             _rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+            _animator.SetTrigger(AnimationSettings.Jump);
         }
 
-        if(Input.GetKey(KeyCode.Space) && _rb.velocity.y < 0)
-        {
-            ChickenGlide(); //Test
-        }
+        //if (Input.GetKey(KeyCode.Space) && _rb.velocity.y < 0)
+        //{
+        //    ChickenGlide(); //Test
+        //}
     }
-    
+
     void ChickenGlide() //Test
     {
         _rb.velocity = new Vector3(_rb.velocity.x, -1f, _rb.velocity.z);
@@ -69,5 +80,16 @@ public class PlayerMovement : MonoBehaviour
             _onGround = false;
         }
     }
+
+
+#if UNITY_EDITOR
+    protected override void OnBindField()
+    {
+        base.OnBindField();
+        _rb = GetComponent<Rigidbody>();
+        _animator = GetComponentInChildren<Animator>();
+
+    }
+#endif
 
 }
